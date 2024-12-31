a720fbb2463fd957e1760ef6dc98a6dce5cd48b7
Esemudia <57021444+Esemudia@users.noreply.github.com>
2025-01-01 00:57:40 +0100

    Resolved conflicts and merged updates

--- node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/server.js
+++ node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 // MongoDB Connection
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.DB_URI, {
         });
         console.log('Connected to MongoDB');
     } catch (error) {
