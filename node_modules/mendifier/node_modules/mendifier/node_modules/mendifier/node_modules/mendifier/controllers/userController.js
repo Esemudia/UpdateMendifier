38d8d77f4957e47ba851508f078ce7660431d991
Esemudia <57021444+Esemudia@users.noreply.github.com>
2025-01-01 12:20:17 +0100

    Resolved conflicts and merged updates

--- node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/controllers/userController.js
+++ node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/node_modules/mendifier/controllers/userController.js
@@ -18,7 +18,7 @@ exports.signUp = async (req, res) => {
 
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        res.status(500).json({ error: 'Error registering user' });
+        res.status(500).json({ error: 'Error registering user'+" "+ error.message });
     }
 };
 
